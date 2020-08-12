8f8f1e80e002846f7d49ff116f06e086a1509b33
Bastien <bmoulia@gmail.com>
2020-08-12 10:02:42 +0200

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UploadComponent } from './upload/upload.component';
+import { DownloadComponent } from './download/download.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    DownloadComponent
   ],
   imports: [
     BrowserModule,
