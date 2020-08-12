414fe15506886f76062714f5285af1aac270bf85
Bastien <bmoulia@gmail.com>
2020-08-12 11:45:12 +0200

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UploadComponent } from './upload/upload.component';
 import { DownloadComponent } from './download/download.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { DownloadComponent } from './download/download.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     NgbModule
   ],
   providers: [],
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as JSZip from 'jszip';
+import saveAs from 'file-saver';
 
 @Component({
   selector: 'app-upload',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
+  files: any[] = [];
+  zip: JSZip;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onFileChange(event: Event): void {
+    const files = (event.target as HTMLInputElement).files;
+    this.openZip(files[0]);
+  }
+
+  openZip(file: File): void {
+    JSZip.loadAsync(file).then((zip) => {
+      console.log(zip);
+      this.zip = zip;
+      this.files = Object.values(zip.files);
+      this.files.forEach(f => f.selected = true);
+    });
+  }
+
+  simulateUpload(): void {
+    this.files.filter(f => !f.selected).forEach(f => {
+      this.zip.remove(f.name);
+    });
+    this.zip.generateAsync({type: 'blob'}).then((blob) => {
+      saveAs(blob, 'generated.zip');
+    });
+  }
 }
