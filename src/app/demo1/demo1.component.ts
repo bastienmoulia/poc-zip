685314173328cbd643112fe05af6eeb7a99fe71c
Bastien <bmoulia@gmail.com>
2020-08-13 10:17:46 +0200

    filter on type file and filename

--- src/app/demo1/demo1.component.ts
+++ src/app/demo1/demo1.component.ts
@@ -5,6 +5,7 @@ import saveAs from 'file-saver';
 interface ZipFile extends JSZip.JSZipObject {
   selected?: boolean;
 }
+const IGNORED_FILES = ['.DS_Store'];
 
 @Component({
   templateUrl: './demo1.component.html',
@@ -26,7 +27,17 @@ export class Demo1Component implements OnInit {
     JSZip.loadAsync(file).then((zip) => {
       console.log(zip);
       this.zip = zip;
-      this.files = Object.values(zip.files);
+      this.files = Object.values(zip.files)
+        .filter((f) => !f.dir)
+        .filter((f) => {
+          let ignore = false;
+          IGNORED_FILES.forEach((ignoredFile) => {
+            if (f.name.indexOf(ignoredFile) > -1) {
+              ignore = true;
+            }
+          });
+          return !ignore;
+        });
       this.files.forEach((f) => (f.selected = true));
     });
   }
